413e023fa6b10eaa520107d8a9f4138dee222f08
Oleksandr Shtonda <oleksandrshtonda@mac.home>
2025-08-07 15:22:03 +0200

    some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+import Input from "./shared/ui/input/input.component";
+
+import GoogleIcon from "./shared/assets/svg/google-icon.svg?react";
+import SearchIcon from "./shared/assets/svg/search-icon.svg?react";
+import EyeOpenIcon from "./shared/assets/svg/eye-open-icon.svg?react";
+import EyeClosedIcon from "./shared/assets/svg/eye-closed-icon.svg?react";
 
 function App() {
-  return <div className="ui-container"></div>;
+  const [text, setText] = useState<string>("");
+
+  return (
+    <div className="ui-container">
+      <Input
+        value={text}
+        setter={setText}
+        RightIcon={EyeOpenIcon}
+        LeftIcon={EyeClosedIcon}
+        label="hehehhe"
+      >
+        Enter text
+      </Input>
+    </div>
+  );
 }
 
 export default App;
--- src/shared/ui/input/input.component.tsx
+++ src/shared/ui/input/input.component.tsx
@@ -1,31 +1,47 @@
-import type {
-  Dispatch,
-  FC,
-  HTMLInputTypeAttribute,
-  SetStateAction,
-} from "react";
+import type { Dispatch, FC, SetStateAction, SVGProps } from "react";
 import "./input.component.scss";
+
+type IconType = FC<SVGProps<SVGSVGElement>>;
 
 interface InputProps {
   setter: Dispatch<SetStateAction<string>>;
   value: string;
-  children: HTMLInputTypeAttribute;
+  children: string;
+  label?: string;
   disabled?: boolean;
+  error?: string;
+  LeftIcon?: IconType;
+  RightIcon?: IconType;
+  paintIcons?: boolean;
 }
 
+// TODO: if only 1 right icon it moves to the left side - bug
 const Input: FC<InputProps> = ({
   setter,
   value,
   children,
+  label,
   disabled = false,
+  error = "",
+  LeftIcon,
+  RightIcon,
+  paintIcons = true,
 }) => {
   return (
-    <input
-      onChange={(e) => setter(e.target.value)}
-      value={value}
-      placeholder={children}
-      disabled={disabled}
-    />
+    <div className={`input-field ${label ? "input-field--has-label" : ""}`}>
+      {label && <label>{label}</label>}
+      {LeftIcon && <LeftIcon className="input__left-icon" />}
+
+      <input
+        onChange={(e) => setter(e.target.value)}
+        className={`input ${LeftIcon ? "input--has-left-icon" : ""}`}
+        value={value}
+        placeholder={children}
+        disabled={disabled}
+      />
+
+      {RightIcon && <RightIcon className="input__right-icon" />}
+    </div>
   );
 };
 
